4000372a98dcb7b10d6240878b2d86e844ca135e
Fanya <ifunnyi1@yandex.ru>
2025-11-20 21:15:43 +0300

    mixins fixx

--- src/main/java/com/fanya/waxedicons/mixin/DrawContextMixin.java
+++ src/main/java/com/fanya/waxedicons/mixin/DrawContextMixin.java
@@ -3,8 +3,10 @@ package com.fanya.waxedicons.mixin;
 import com.fanya.waxedicons.util.WaxedBlocks;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,11 +17,8 @@ import java.util.Objects;
 @Mixin(DrawContext.class)
 public class DrawContextMixin {
 
-    @Inject(
-            method = "drawItemWithoutEntity*",
-            at = @At("RETURN")
-    )
-    private void afterDrawItem(ItemStack stack, int x, int y, CallbackInfo ci) {
+    @Inject(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At("RETURN"))
+    private void afterDrawItem(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, int z, CallbackInfo ci) {
         if (stack != null && !stack.isEmpty() && WaxedBlocks.WAXED_BLOCKS.contains(stack.getItem())) {
             Identifier iconTexture = WaxedBlocks.getCustomIcon();
             int size = Objects.equals(iconTexture, Identifier.of("waxedicons", "textures/gui/waxed_icon_alternative.png")) ? 8 : 6;
